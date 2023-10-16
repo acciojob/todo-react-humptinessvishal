5ee73856a3cf7c84ec51dfebfdd6b856fb98893b
Vishal Kumar <136982736+humptinessvishal@users.noreply.github.com>
2023-10-16 14:54:11 +0000

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
-import ToDoList from "./ToDoList"
+// import ToDoList from "./ToDoList"
 
 const App = () => {
+  // return (
+  //   <div>
+  //     {/* Do not remove the main div */}
+  //     <ToDoList />
+  //   </div>
+  // )
+  const [value, setValue] = useState("");
+  const [todo, setTodo] = useState([]);
+
+  function addItem() {
+    setTodo([...todo, value]);
+    setValue("");
+  }
+
+  function deleteItem(index) {
+    let newTodo = [...todo];
+    // newTodo.splice(index, 1);
+    setTodo(todo.filter((value, position)=>  position !== index));
+    // setTodo(newTodo);
+  }
+
   return (
     <div>
-      {/* Do not remove the main div */}
-      <ToDoList />
+      <h1>To-Do List</h1>
+      <div>
+        <input type="text" onChange={(event) => setValue(event.target.value)} value={value} />
+        <button onClick={addItem} >Add Todo</button>
+      </div>
+      <ul className="todo-container">
+        {
+          todo.map((item, index) => (
+            <li className="value-container">
+              <p>{item}</p>
+              <button onClick={() => deleteItem(index)}>Delete</button>
+            </li>
+          ))
+        }
+      </ul>
     </div>
   )
 }
